2353822b81f432d0a3012c41b68bc5b20569cf30
Ken Layug <ken_layug@yahoo.com>
2016-02-18 11:28:03 +0800

    validations

--- app/controllers/ImageReportController.php
+++ app/controllers/ImageReportController.php
@@ -4,27 +4,38 @@
 
 		public function reportImage(){
 
-			$milliseconds = round(microtime(true) * 1000);
-			$fileName = $milliseconds.".".Input::file('fileImage')->getClientOriginalExtension();
-			$path = public_path() . "/reported_images/" ;
-			// $pos = strpos($path,'/public/');
-			// if ($pos !== false) {
-			// 	var_dump("HEEEERE");
-			//     $path = substr($path, $milliseconds);
-			// }
-			Input::file('fileImage')->move($path, $fileName);
+			$validator = Validator::make(
+					['fileImage' => 'image'],
+					['fileImage' => 'max:10000']);
 
-			$strName = Request::input('name');
-			$strRemarks = Request::input('remarks');
+			if (!$validator->fails()){
 
-			DB::table('Report')
-				->insert([
-						'imageReport' => $path.$fileName,
-						'name' => $strName,
-						'remarks' => $strRemarks
-					]);
+				$milliseconds = round(microtime(true) * 1000);
+				$fileName = $milliseconds.".".Input::file('fileImage')->getClientOriginalExtension();
+				$path = public_path() . "/reported_images/" ;
+				// $pos = strpos($path,'/public/');
+				// if ($pos !== false) {
+				// 	var_dump("HEEEERE");
+				//     $path = substr($path, $milliseconds);
+				// }
+				Input::file('fileImage')->move($path, $fileName);
 
-			echo "success";
+				$strName = Request::input('name');
+				$strRemarks = Request::input('remarks');
+
+				DB::table('Report')
+					->insert([
+							'imageReport' => $path.$fileName,
+							'name' => $strName,
+							'remarks' => $strRemarks
+						]);
+
+				echo "success";
+
+			}
+			else{
+				echo "boom";
+			}
 
 		}
 
--- app/controllers/UrlReportController.php
+++ app/controllers/UrlReportController.php
@@ -4,17 +4,24 @@
 
 		public function createReportURL(){
 
-			$strName = Request::input('name');
-			$strUrl = Request::input('url');
-			$strRemarks = Request::input('remarks');
+			$validator = Validator::make([
+					'url' => 'url'
+				]);
 
-			DB::table('Report')
-				->insert([
-						'name' => $strName,
-						'urlReport' => $strUrl,
-						'datReport' => date('m/d/Y h:i:s a', time()),
-						'remarks' => $strRemarks
-					]);
+			if (!$validator->fails()){
+				$strName = Request::input('name');
+				$strUrl = Request::input('url');
+				$strRemarks = Request::input('remarks');
+
+				DB::table('Report')
+					->insert([
+							'name' => $strName,
+							'urlReport' => $strUrl,
+							'datReport' => date('m/d/Y h:i:s a', time()),
+							'remarks' => $strRemarks
+						]);
+
+			}
 
 		}
 
