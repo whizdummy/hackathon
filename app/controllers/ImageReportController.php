a3feb6a4959970384fc14073fe7a0819484da2e9
Ken Layug <ken_layug@yahoo.com>
2016-02-19 09:16:20 +0800

    changes.

--- app/controllers/ImageReportController.php
+++ app/controllers/ImageReportController.php
@@ -4,27 +4,38 @@
 
 		public function reportImage(){
 
-			$validator = Validator::make(
+			if (Request::input('url') != null){
+
+				$strUrl = Request::input('url');
+
+			}
+			if (Request::input('fileImage') != null){
+
+				$validator = Validator::make(
 					['fileImage' => 'image'],
 					['fileImage' => 'max:10000']);
 
-			if (!$validator->fails()){
+				if (!$validator->fails()){
 
-				if (strlen(Request::input('name')) < 0){
+					if (strlen(Request::input('name')) < 0){
 
-					//call error
+						//call error
 
-				}
-				$milliseconds = round(microtime(true) * 1000);
-				$fileName = $milliseconds.".".Input::file('fileImage')->getClientOriginalExtension();
-				$path = public_path() . "/reported_images/" ;
-				// $pos = strpos($path,'/public/');
-				// if ($pos !== false) {
-				// 	var_dump("HEEEERE");
-				//     $path = substr($path, $milliseconds);
-				// }
-				Input::file('fileImage')->move($path, $fileName);
+					}
+					$milliseconds = round(microtime(true) * 1000);
+					$fileName = $milliseconds.".".Input::file('fileImage')->getClientOriginalExtension();
+					$path = public_path() . "/reported_images/" ;
+					// $pos = strpos($path,'/public/');
+					// if ($pos !== false) {
+					// 	var_dump("HEEEERE");
+					//     $path = substr($path, $milliseconds);
+					// }
+					Input::file('fileImage')->move($path, $fileName);
 
+
+			}
+
+			
 				$strName = Request::input('name');
 				$strRemarks = Request::input('remarks');
 
